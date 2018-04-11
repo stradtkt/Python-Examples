2496ae767b003f32074e0f7379bd2abdcc23e18b
Kevin <stradtkt22@gmail.com>
2018-04-11 00:05:59 -0400

    Initial Commit

---
+++ PracticePY.py
@@ -0,0 +1,71 @@
+# name = input("Please enter your name: ")
+# age = int(input("How old are you, {0}? ".format(name)))
+# print(age)
+
+# if age >= 18:
+#   print("You are old enough to vote")
+# elif age >= 21:
+#   print("You are old enough to drink and vote like a true American")
+# else:
+#   print("Your age, {0}, is not old enough to do anything.".format(age))
+
+# for i in range(1, 14):
+#   print("{0}".format(i))
+
+# for i in range(1,10):
+#   print("i is now {0}".format(i))
+
+# number = "9,233,372,036,987"
+# cleanedNumber = ''
+# for i in range(0, len(number)):
+#   if number[i] in '1234567890':
+#     cleanedNumber = cleanedNumber + number[i]
+#     print(number[i], end='')
+# newNumber = int(cleanedNumber)
+# print("The number is {} ".format(newNumber))
+
+# number = "9,322,456,789"
+# cleanedNumber = ''
+
+# for char in number:
+#   if char in '01234567890':
+#     cleanedNumber = cleanedNumber + char
+# newNumber = int(cleanedNumber)
+
+# print("The number is {}".format(newNumber))
+
+
+# number = "9,234,432,234,432"
+# cleanedNumber = ''
+# for i in range(0, len(number)):
+#   if number[i] in '0987654321':
+#     cleanedNumber += number[i] ##more efficient then cleanedNumber = cleanedNumber + number
+
+# newNumber = int(cleanedNumber)
+# print("The number is {} ".format(newNumber))
+
+# x = 23 
+# x +=21
+# print(x)
+
+# x-=4
+# print(x)
+
+# x *= 5
+# print(x)
+
+# x/=4 
+# print(x)
+
+# x**=2
+# print(x)
+
+# x%=60
+# print(x)
+
+# greeting = "Good"
+# greeting += " morning!"
+# print(greeting)
+
+# greeting*= 5
+# print(greeting)
