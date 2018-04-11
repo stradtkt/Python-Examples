11f32f1914950e60f48cd8700352d9febdebe9b0
Kevin <stradtkt22@gmail.com>
2018-04-11 09:46:51 -0400

    Added iterators and ranges examples

---
+++ iterators/iterators.py
@@ -0,0 +1,26 @@
+# string = '123456789'
+# for char in string:
+#   print(char)
+# for char in string:
+#   print(char)
+# for char in iter(string):
+#   print(char)
+
+
+# my_iterator = iter(string)
+# print(my_iterator)
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+
+# my_list = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+
+# my_iterator = iter(my_list)
+# for i in range(0, len(my_list)):
+#   next_item = next(my_iterator)
+#   print(next_item)
---
+++ ranges/ranges.py
@@ -0,0 +1,37 @@
+# my_list = list(range(10))
+# print(my_list)
+
+# even = list(range(0, 10, 2))
+# odd = list(range(1,10,2))
+# print(even)
+# print(odd)
+
+
+# my_string = "abcdefghijklmnopqrstuvwxyz"
+# print(my_string.index('e'))
+# print(my_string[4])
+
+# small_decimals = range(0,10)
+# print(small_decimals)
+# print(small_decimals.index(3))
+
+# odd = range(1, 1000, 2)
+# print(odd)
+
+# sevens = range(7, 1000, 7)
+# x = int(input("Please enter a positive number within 1000"))
+# if x in sevens:
+#   print("{} is divisible by seven".format(x))
+
+# decimals = range(0, 100)
+# print(decimals)
+
+# my_range = decimals[3:40:3]
+# print(my_range)
+
+# for i in my_range:
+#   print(i)
+
+# print('=' * 40)
+# for i in range(3, 40, 3):
+#   print(i)
