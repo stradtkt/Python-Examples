3ff9ecc5d87e3d237f7046de1b1a855bfa05d0eb
Kevin <stradtkt22@gmail.com>
2018-04-12 15:58:30 -0400

    Added dir functions to show the library for items

---
+++ modules/StandardLibrary/standard_library.py
@@ -0,0 +1,18 @@
+# print(dir())
+
+# # print(dir(__builtins__))
+
+# for m in dir(__builtins__):
+#   print(m)
+
+# import shelve 
+# print(dir())
+# print()
+# print(dir(shelve))
+
+# for obj in dir(shelve.Shelf):
+#   if obj[0] != '_':
+#     print(obj)
+# shelve.Shelf.
+
+# help(shelve)
