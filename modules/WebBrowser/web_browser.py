f76ce9e94bd5b478689c49303bc36a9e3802f4e1
Kevin <stradtkt22@gmail.com>
2018-04-12 16:34:13 -0400

    Added webbrowser examples

---
+++ modules/WebBrowser/web_browser.py
@@ -0,0 +1,10 @@
+import webbrowser
+
+# webbrowser.open("https://google.com")
+# help(webbrowser)
+
+# for i in range(10):
+#   print(1, 2, 3, 4, 5, 6, 7, 8, 9, sep=':', end=' ')
+
+chrome = webbrowser.open_new_tab('https://github.com')
+
