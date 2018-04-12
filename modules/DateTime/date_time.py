a4ebf64a32eaa8260a9761e8051ac2a8793f1b4a
Kevin <stradtkt22@gmail.com>
2018-04-12 18:10:27 -0400

    added timezones file with some others

--- modules/DateTime/date_time.py
+++ modules/DateTime/date_time.py
@@ -10,7 +10,7 @@
 # print("Month: ", time_here[1], time_here.tm_mon)
 # print("Day: ", time_here[2], time_here.tm_mday)
 import time
-from time import time as my_timer
+from time import perf_counter as my_timer
 import random
 input("Please enter to start")
 wait_time = random.randint(1,6)
@@ -22,3 +22,4 @@ end_time = my_timer()
 print("Started at " + time.strftime("%X", time.localtime(start_time)))
 print("Ended at " + time.strftime("%X", time.localtime(end_time)))
 print("Your reaction time was {} seconds".format(end_time - start_time))
+
---
+++ treehouse/files/file.py
@@ -0,0 +1,14 @@
+# import os
+# os.getcwd()
+# os.path.join(os.getcwd,  '...' , 'treehouse')
+# import pathlib
+# path = pathlib.PurePath(os.getcwd())
+# path2 = path / 'examples' / 'paths.txt'
+# path2.parts
+# path2.root
+# path2.parent[2]
+# path2.name
+# path2.suffix
+import os
+os.listdir()
+
